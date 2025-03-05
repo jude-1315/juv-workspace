c7bb6023ed38698df1dcbf893d46768d5854aec2
Jude K <itsjudekuti@gmail.com>
2025-03-04 22:11:11 -0600

    Init setup for multiple repo workspace

---
+++ projects/juv-ionic/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.juv.ionic.app',
+  appName: 'juv-ionic',
+  webDir: 'www'
+};
+
+export default config;
